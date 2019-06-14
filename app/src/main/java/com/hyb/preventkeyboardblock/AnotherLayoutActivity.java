06f262fad4b85b03f2191f0f1e8191ac6eb44e61
358947416@qq.com <358947416@qq.com>
2019-06-14 18:02:29 +0800

    a

--- app/src/main/java/com/hyb/preventkeyboardblock/AnotherLayoutActivity.java
+++ app/src/main/java/com/hyb/preventkeyboardblock/AnotherLayoutActivity.java
@@ -1,24 +1,33 @@
 package com.hyb.preventkeyboardblock;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 
 import com.hyb.library.PreventKeyboardBlockUtil;
 
+
 public class AnotherLayoutActivity extends Activity {
 
-
+    Button btnView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anotherlayout);
+        btnView = findViewById(R.id.btn_submit);
 
-        Button btnView = findViewById(R.id.btn_submit);
-        ConstraintLayout contentLayout = findViewById(R.id.cl_content);
-        PreventKeyboardBlockUtil.getInstance(this).setBtnView(btnView).setContentLayout(contentLayout).register();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        PreventKeyboardBlockUtil.getInstance(this).setBtnView(btnView).register();
     }
 }
--- app/src/main/java/com/hyb/preventkeyboardblock/ConstraintLayoutActivity.java
+++ app/src/main/java/com/hyb/preventkeyboardblock/ConstraintLayoutActivity.java
@@ -1,8 +1,15 @@
 package com.hyb.preventkeyboardblock;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.app.Activity;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,7 +19,7 @@ import com.hyb.library.PreventKeyboardBlockUtil;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ConstraintLayoutActivity extends AppCompatActivity {
+public class ConstraintLayoutActivity extends Activity {
 
     @BindView(R.id.et_account)
     EditText etAccount;
@@ -31,7 +38,7 @@ public class ConstraintLayoutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        PreventKeyboardBlockUtil.getInstance(this).setBtnView(btnSubmit).setContentLayout(clContent).register();
-
+        PreventKeyboardBlockUtil.getInstance(this).setBtnView(btnSubmit).register();
     }
+
 }
